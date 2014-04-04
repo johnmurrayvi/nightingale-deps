1593a85e6f907ff317fd5ea15a80056f987ee9d2
John Murray <johnmurrayvi@gmail.com>
2014-04-04 10:30:09 -0400

    script to build from inside build/ dir

---
+++ make-cross.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH="/opt/mxe/bin:$PATH"
+
+export CROSS=i686-pc-mingw32.static-
+
+export CC=$(CROSS)gcc
+export LD=$(CROSS)ld
+export AR=$(CROSS)ar
+export PKG_CONFIG=$(CROSS)pkg-config
+
+make 
