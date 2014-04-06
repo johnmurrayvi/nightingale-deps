977fbb11001153ee398804b9d99f447d66064df6
John Murray <johnmurrayvi@gmail.com>
2014-04-05 21:43:31 -0400

    update make-cross lazy script for new path

--- make-cross.sh
+++ make-cross.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-export PATH="/opt/mxe/bin:$PATH"
+# export PATH="/opt/mxe/bin:$PATH"
+export PATH="/opt/cross/mxe/usr/bin:$PATH"
 
-export CROSS=i686-pc-mingw32.static-
+export CROSS="i686-pc-mingw32.static-"
 
-export CC=$(CROSS)gcc
-export LD=$(CROSS)ld
-export AR=$(CROSS)ar
-export PKG_CONFIG=$(CROSS)pkg-config
+export CC="$CROSS"gcc
+export LD="$CROSS"ld
+export AR="$CROSS"ar
+export PKG_CONFIG="$CROSS"pkg-config
 
-make 
+make
