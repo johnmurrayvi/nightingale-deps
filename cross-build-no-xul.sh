972807b064168d05d133624cf252ce6ec7d85220
John Murray <johnmurrayvi@gmail.com>
2014-04-07 00:52:23 -0400

    reorganize/tweak cross build script

--- cross-build-no-xul.sh
+++ cross-build-no-xul.sh
@@ -101,7 +101,7 @@ case $OSTYPE in
 #
 #        ### TIER 3 ###
 #        # echo -e "Building gstreamer...\n"
-       make -C gstreamer -f Makefile.ngmingw32
+#        make -C gstreamer -f Makefile.ngmingw32
 #
 #        # export CROSS_LIBTYPE="static"
 #        # make -C gstreamer -f Makefile.ngmingw32
@@ -128,57 +128,80 @@ case $OSTYPE in
 
 
 
-#         export CROSS_LIBTYPE="shared"
-# # ------------------------------------------------------- #
+        export CROSS_LIBTYPE="shared"
+## ------------------------------------------------------- #
+#
+#        # echo -e "Building libiconv...\n"
+#        make -C libiconv -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        # echo -e "Building libjpeg-turbo...\n"
+#        # make -C libjpeg-turbo -f Makefile.ngmingw32
+#
+#        # echo -e "Building zlib...\n"
+#        # make -C zlib -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        ### TIER 1 ###
+#        # echo -e "Building gettext...\n"
+#        make -C gettext -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        # echo -e "Building glib...\n"
+#        make -C glib -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        # echo -e "Building sqlite...\n"
+#        make -C sqlite -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        ### TIER 2 ###
+#        # echo -e "Building libogg...\n"
+#        make -C libogg -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        # echo -e "Building libvorbis...\n"
+#        make -C libvorbis -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        # echo -e "Building libtheora...\n"
+#        make -C libtheora -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        # echo -e "Building FLAC...\n"
+#        make -C flac -f Makefile.ngmingw32
+#
+## ------------------------------------------------------- #
+#
+#        ### TIER 3 ###
+#        # echo -e "Building gstreamer...\n"
+#        # make -C gstreamer -f Makefile.ngmingw32
+#
+#        # export CROSS_LIBTYPE="static"
+#        # make -C gstreamer -f Makefile.ngmingw32
+#        # mv build/gstreamer build/gstreamer-static
+#        # export CROSS_LIBTYPE="shared"
+#        # make -C gstreamer -f Makefile.ngmingw32
+#        # mv build/gstreamer build/gstreamer-shared
+#
+## ------------------------------------------------------- #
+#
+#        ### TIER 4 ###
+#        # echo -e "Building gst plugins...\n"
+#        # make -C gst-plugins-base -f Makefile.ngmingw32
+#        # make -C gst-plugins-good -f Makefile.ngmingw32
+#        # make -C gst-plugins-bad -f Makefile.ngmingw32
+#        # make -C gst-plugins-ugly -f Makefile.ngmingw32
 
-#         # echo -e "Building libiconv...\n"
-#         make -C libiconv -f Makefile.ngmingw32
 
-# # ------------------------------------------------------- #
-
-#         # echo -e "Building libjpeg-turbo...\n"
-#         # make -C libjpeg-turbo -f Makefile.ngmingw32
-
-#         # echo -e "Building zlib...\n"
-#         # make -C zlib -f Makefile.ngmingw32
-
-# # ------------------------------------------------------- #
-
-#         ### TIER 1 ###
-#         # echo -e "Building gettext...\n"
-#         make -C gettext -f Makefile.ngmingw32
-
-# # ------------------------------------------------------- #
-
-#         # echo -e "Building glib...\n"
-#         # make -C glib -f Makefile.ngmingw32
-#         make -C glib-2.40.0 -f Makefile.ngmingw32
-
-# # ------------------------------------------------------- #
-
-#         # echo -e "Building sqlite...\n"
-#         make -C sqlite -f Makefile.ngmingw32
-
-# # ------------------------------------------------------- #
-
-#         ### TIER 2 ###
-#         # echo -e "Building libogg...\n"
-#         make -C libogg -f Makefile.ngmingw32
-
-# # ------------------------------------------------------- #
-
-#         # echo -e "Building libvorbis...\n"
-#         make -C libvorbis -f Makefile.ngmingw32
-
-# # ------------------------------------------------------- #
-
-#         # echo -e "Building libtheora...\n"
-#         make -C libtheora -f Makefile.ngmingw32
-
-# # ------------------------------------------------------- #
-
-#         # echo -e "Building FLAC...\n"
-#         make -C flac -f Makefile.ngmingw32
     ;;
 
     *)
