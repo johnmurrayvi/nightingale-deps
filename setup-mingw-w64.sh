24e93960e2d66943d9abac183c76d900f977775b
John Murray <johnmurrayvi@gmail.com>
2014-04-19 20:13:57 -0400

    add creation of cmake toolchain file to mingw-w64 script

--- setup-mingw-w64.sh
+++ setup-mingw-w64.sh
@@ -148,7 +148,7 @@ cd $TOPDIR
 # set up pkg-config
 PKGCONF=i686-w64-mingw32-pkg-config
 
-(cat << EOF) > $PREFIX/bin/$PKGCONF
+(cat << EOF) > $PKGCONF
 #!/bin/bash
 
 # This file has no copyright assigned and is placed in the Public Domain.
@@ -167,7 +167,49 @@ export PKG_CONFIG_PATH=$PKG_CONFIG_PATH_MINGWW64
 pkg-config $@
 EOF
 
+sudo mv $PKGCONF $PREFIX/bin/$PKGCONF
 sudo chmod +x $PREFIX/bin/$PKGCONF
+
+
+# install cmake toolchain file
+CTCFILE=mingw-w64-conf.cmake
+
+sudo mkdir $PREFIX/i686-w64-mingw32/share/cmake
+
+(cat << EOF) > $CTCFILE
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+set(MSYS 1)
+set(BUILD_SHARED_LIBS ON)
+
+# Choose an appropriate compiler prefix
+# for 32 or 64 bits mingw-w64
+# see http://mingw-w64.sourceforge.net/
+set(COMPILER_PREFIX "i686-w64-mingw32")
+
+# which compilers to use for C and C++
+#find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
+#find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
+#find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
+set(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
+set(CMAKE_RC_COMPILER ${COMPILER_PREFIX}-windres)
+set(PKG_CONFIG_EXECUTABLE ${COMPILER_PREFIX}-pkg-config)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH /opt/cross/mingw-w64/${COMPILER_PREFIX})
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+EOF
+
+sudo mv $CTCFILE $PREFIX/i686-w64-mingw32/share/cmake/$CTCFILE
+
 
 
 echo "done!"
