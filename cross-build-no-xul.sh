70df999afc749d641f771eb7f18765652c7d878c
John Murray <johnmurrayvi@gmail.com>
2014-04-20 01:22:57 -0400

    support both mingw-w64 (preferred right now) and mxe

--- cross-build-no-xul.sh
+++ cross-build-no-xul.sh
@@ -21,12 +21,17 @@ export NG_VENDOR_CROSS_COMP=1
 # export CROSS="i586-pc-mingw32msvc-"
 
 # mxe
-# export PATH="/opt/mxe/bin:$PATH"
-export PATH="/opt/cross/mxe/usr/bin:$PATH"
+# export PATH="/opt/cross/mxe/usr/bin:$PATH"
+# echo $PATH
+# export CROSS="i686-pc-mingw32"
+# export CROSS_LIBTYPE="static"
+# export CROSS_CMAKE_TC_PATH="/opt/cross/mxe/usr"
+
+# mingw-w64
+export PATH="/opt/cross/mingw-w64/bin:$PATH"
 echo $PATH
-export CROSS="i686-pc-mingw32"
-export CROSS_LIBTYPE="static"
-export CROSS_CMAKE_TC_PATH="/opt/cross/mxe/usr"
+export CROSS="i686-w64-mingw32"
+export CROSS_CMAKE_TC_PATH="/opt/cross/mingw-w64"
 
 
 if [ ! -d "build" ]; then
@@ -43,126 +48,178 @@ case $OSTYPE in
         fi
 
 
+        if [ "$CROSS" == "i686-pc-mingw32" ]
+
+            export CROSS_LIBTYPE="static"
+    # ------------------------------------------------------- #
+           echo -e "Building libiconv...\n"
+           make -C libiconv -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+
+           ### TIER 1 ###
+           echo -e "Building gettext...\n"
+           make -C gettext -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building zlib...\n"
+           make -C zlib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building glib...\n"
+           make -C glib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building sqlite...\n"
+           make -C sqlite -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libjpeg-turbo...\n"
+           make -C libjpeg-turbo -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 2 ###
+           echo -e "Building libogg...\n"
+           make -C libogg -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libvorbis...\n"
+           make -C libvorbis -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libtheora...\n"
+           make -C libtheora -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building FLAC...\n"
+           make -C flac -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building taglib...\n"
+           make -C taglib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 3 ###
+           echo -e "Building gstreamer...\n"
+           make -C gstreamer -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 4 ###
+           echo -e "Building gst plugins...\n"
+           make -C gst-plugins-base -f Makefile.ngmingw32
+           make -C gst-plugins-good -f Makefile.ngmingw32
+           make -C gst-plugins-bad -f Makefile.ngmingw32
+           make -C gst-plugins-ugly -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+
+           mv build build-static
+           mv windows-i686-msvc10 windows-i686-msvc10-static
+    
+           export CROSS_LIBTYPE="shared"
+    # ------------------------------------------------------- #
+           echo -e "Building libiconv...\n"
+           make -C libiconv -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 1 ###
+           echo -e "Building gettext...\n"
+           make -C gettext -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building glib...\n"
+           make -C glib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building zlib...\n"
+           make -C zlib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building sqlite...\n"
+           make -C sqlite -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libjpeg-turbo...\n"
+           make -C libjpeg-turbo -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 2 ###
+           echo -e "Building libogg...\n"
+           make -C libogg -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libvorbis...\n"
+           make -C libvorbis -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libtheora...\n"
+           make -C libtheora -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building FLAC...\n"
+           make -C flac -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building taglib...\n"
+           make -C taglib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 3 ###
+           echo -e "Building gstreamer...\n"
+           make -C gstreamer -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 4 ###
+           echo -e "Building gst plugins...\n"
+           make -C gst-plugins-base -f Makefile.ngmingw32
+           make -C gst-plugins-good -f Makefile.ngmingw32
+           make -C gst-plugins-bad -f Makefile.ngmingw32
+           make -C gst-plugins-ugly -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           mv build build-shared
+           mv windows-i686-msvc10 windows-i686-msvc10-shared
 
 
-        export CROSS_LIBTYPE="static"
-# ------------------------------------------------------- #
-       echo -e "Building libiconv...\n"
-       make -C libiconv -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 1 ###
-       echo -e "Building gettext...\n"
-       make -C gettext -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building glib...\n"
-       make -C glib -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building zlib...\n"
-       make -C zlib -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building sqlite...\n"
-       make -C sqlite -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building libjpeg-turbo...\n"
-       make -C libjpeg-turbo -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 2 ###
-       echo -e "Building libogg...\n"
-       make -C libogg -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building libvorbis...\n"
-       make -C libvorbis -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building libtheora...\n"
-       make -C libtheora -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building FLAC...\n"
-       make -C flac -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building taglib...\n"
-       make -C taglib -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 3 ###
-       echo -e "Building gstreamer...\n"
-       make -C gstreamer -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 4 ###
-       echo -e "Building gst plugins...\n"
-       make -C gst-plugins-base -f Makefile.ngmingw32
-       make -C gst-plugins-good -f Makefile.ngmingw32
-       make -C gst-plugins-bad -f Makefile.ngmingw32
-       make -C gst-plugins-ugly -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       mv build build-static
-       mv windows-i686-msvc10 windows-i686-msvc10-static
-
-
-
-
-        export CROSS_LIBTYPE="shared"
-# ------------------------------------------------------- #
-       echo -e "Building libiconv...\n"
-       make -C libiconv -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 1 ###
-       echo -e "Building gettext...\n"
-       make -C gettext -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building glib...\n"
-       make -C glib -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building zlib...\n"
-       make -C zlib -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building sqlite...\n"
-       make -C sqlite -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building libjpeg-turbo...\n"
-       make -C libjpeg-turbo -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 2 ###
-       echo -e "Building libogg...\n"
-       make -C libogg -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building libvorbis...\n"
-       make -C libvorbis -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building libtheora...\n"
-       make -C libtheora -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building FLAC...\n"
-       make -C flac -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-       echo -e "Building taglib...\n"
-       make -C taglib -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 3 ###
-       echo -e "Building gstreamer...\n"
-       make -C gstreamer -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-       ### TIER 4 ###
-       echo -e "Building gst plugins...\n"
-       make -C gst-plugins-base -f Makefile.ngmingw32
-       make -C gst-plugins-good -f Makefile.ngmingw32
-       make -C gst-plugins-bad -f Makefile.ngmingw32
-       make -C gst-plugins-ugly -f Makefile.ngmingw32
-# ------------------------------------------------------- #
-
-        mv build build-shared
-        mv windows-i686-msvc10 windows-i686-msvc10-shared
+        else
+    
+    # ------------------------------------------------------- #
+           echo -e "Building libiconv...\n"
+           make -C libiconv -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 1 ###
+           echo -e "Building gettext...\n"
+           make -C gettext -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building zlib...\n"
+           make -C zlib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building glib...\n"
+           make -C glib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building sqlite...\n"
+           make -C sqlite -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libjpeg-turbo...\n"
+           make -C libjpeg-turbo -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 2 ###
+           echo -e "Building libogg...\n"
+           make -C libogg -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libvorbis...\n"
+           make -C libvorbis -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building libtheora...\n"
+           make -C libtheora -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building FLAC...\n"
+           make -C flac -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+           echo -e "Building taglib...\n"
+           make -C taglib -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 3 ###
+           echo -e "Building gstreamer...\n"
+           make -C gstreamer -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+    
+           ### TIER 4 ###
+           echo -e "Building gst plugins...\n"
+           make -C gst-plugins-base -f Makefile.ngmingw32
+           make -C gst-plugins-good -f Makefile.ngmingw32
+           make -C gst-plugins-bad -f Makefile.ngmingw32
+           make -C gst-plugins-ugly -f Makefile.ngmingw32
+    # ------------------------------------------------------- #
+        fi
 
         echo "Done!"
         echo "Check windows-i686-msvc10-static and windows-i686-msvc10-shared!"
-
     ;;
 
     *)
