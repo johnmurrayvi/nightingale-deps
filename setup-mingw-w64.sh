fc7af8cd0a73eeef109e00e36c0b3aa3a011c232
John Murray <johnmurrayvi@gmail.com>
2014-04-23 11:42:51 -0400

    fix a few things in the mingw-w64 script

--- setup-mingw-w64.sh
+++ setup-mingw-w64.sh
@@ -31,7 +31,7 @@ JNUM="$(($NUMPROC / 2))"
 PREFIX=/opt/cross/mingw-w64
 
 # NEEDED TO INSTALL CRT AND FINSIH GCC INSTALL
-alias sudowpath="sudo env PATH=$PATH"
+sudowpath="sudo env PATH=$PATH"
 
 
 echo "Your build directory will be $TOPDIR"
@@ -113,14 +113,14 @@ mkdir mingw-w64-crt32 && cd mingw-w64-crt32
     --prefix=$PREFIX/i686-w64-mingw32/
 
 make -j$JNUM
-sudowpath make install
+$sudowpath make install
 cd $TOPDIR
 
 
 # finish GCC build and install
 cd $GCCVER-mingw32
 make -j$JNUM
-sudowpath make install
+$sudowpath make install
 cd $TOPDIR
 
 
@@ -131,7 +131,7 @@ mkdir widl32 && cd widl32
     --target=i686-w64-mingw32
 
 make -j$JNUM
-sudowpath make install
+$sudowpath make install
 cd $TOPDIR
 
 
@@ -188,16 +188,16 @@ set(BUILD_SHARED_LIBS ON)
 set(COMPILER_PREFIX "i686-w64-mingw32")
 
 # which compilers to use for C and C++
-#find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
-#find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
-#find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
-set(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
-set(CMAKE_RC_COMPILER ${COMPILER_PREFIX}-windres)
-set(PKG_CONFIG_EXECUTABLE ${COMPILER_PREFIX}-pkg-config)
+#find_program(CMAKE_RC_COMPILER NAMES \${COMPILER_PREFIX}-windres)
+#find_program(CMAKE_C_COMPILER NAMES \${COMPILER_PREFIX}-gcc)
+#find_program(CMAKE_CXX_COMPILER NAMES \${COMPILER_PREFIX}-g++)
+set(CMAKE_C_COMPILER \${COMPILER_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER \${COMPILER_PREFIX}-g++)
+set(CMAKE_RC_COMPILER \${COMPILER_PREFIX}-windres)
+set(PKG_CONFIG_EXECUTABLE \${COMPILER_PREFIX}-pkg-config)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH /opt/cross/mingw-w64/${COMPILER_PREFIX})
+SET(CMAKE_FIND_ROOT_PATH /opt/cross/mingw-w64/\${COMPILER_PREFIX})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
